a2a2de07f4e48f7d919145a453d5fe04c54ee8db
Christian Galvez <christian.galvez@unbounce.com>
2023-01-09 16:01:09 -0700

    Unit testing for the product list component and products service

--- src/app/core/services/products/products-api.service.spec.ts
+++ src/app/core/services/products/products-api.service.spec.ts
@@ -1,16 +1,58 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ProductsApiService } from './products-api.service';
+import { Product } from "../../models/product";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { of } from "rxjs";
+import { TestScheduler } from "rxjs/testing";
+import { ApiResponse } from "../../models/api-response";
+import SpyObj = jasmine.SpyObj;
+
+const testScheduler = new TestScheduler((actual, expected) => {
+  expect(actual).toEqual(expected);
+});
 
 describe('ProductsApiService', () => {
   let service: ProductsApiService;
+  let mockedProductsApiService: SpyObj<ProductsApiService>;
+  const mockedProduct: Product = { title: 'Product 1', brand: 'Brand 1', price: 59.99, sku: '73526212', stock: 1 };
+  const mockedResponse: ApiResponse<Product[]> = { data: [mockedProduct], success: true };
+  const mockedPurchaseResponse: ApiResponse<[]> = { data: [], success: true };
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    mockedProductsApiService = jasmine.createSpyObj(ProductsApiService, ['getProducts', 'purchase']);
+    mockedProductsApiService.getProducts.and.returnValue(of(mockedResponse));
+    mockedProductsApiService.purchase.and.returnValue(of(mockedPurchaseResponse));
+
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ],
+      providers: [
+        {
+          provide: ProductsApiService,
+          useValue: mockedProductsApiService
+        }
+      ]
+    });
     service = TestBed.inject(ProductsApiService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should get a list of products', () => {
+    testScheduler.run(({hot, expectObservable}) => {
+      const expected = hot('(a|)', { a: mockedResponse });
+      expectObservable(service.getProducts()).toEqual(expected);
+    });
+  });
+
+  it('should purchase a product', () => {
+    testScheduler.run(({hot, expectObservable}) => {
+      const expected = hot('(a|)', { a: mockedPurchaseResponse });
+      expectObservable(service.purchase([mockedProduct.sku])).toEqual(expected);
+    });
   });
 });
--- src/app/features/home/pages/products-list/products-list.component.spec.ts
+++ src/app/features/home/pages/products-list/products-list.component.spec.ts
@@ -1,17 +1,46 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProductsListComponent } from './products-list.component';
+import { MockStore, provideMockStore } from "@ngrx/store/testing";
+import '/src/app/core/utils/deep-copy';
+import { By } from "@angular/platform-browser";
+import { TestScheduler } from "rxjs/testing";
+import { ProductState } from "../../../../state/reducers/product.reducer";
+import { selectAllProducts } from "../../../../state/selectors/product.selectors";
+import { Product } from "../../../../core/models/product";
+import { SharedModule } from "../../../../shared/shared.module";
+import { ProductShowcaseComponent } from "../../../../shared/components/product-showcase/product-showcase.component";
+
+const testScheduler = new TestScheduler((actual, expected) => {
+  expect(actual).toEqual(expected);
+});
 
 describe('ProductsListComponent', () => {
   let component: ProductsListComponent;
   let fixture: ComponentFixture<ProductsListComponent>;
+  let store: MockStore;
+  const initialState: ProductState = { products: [] };
+  const mockProduct: Product = { title: 'Product 1', brand: 'Brand 1', price: 59.99, sku: '73526212', stock: 1 };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ProductsListComponent ]
+      declarations: [ProductsListComponent],
+      imports: [SharedModule],
+      providers: [
+        provideMockStore({
+          initialState,
+          selectors: [
+            {
+              selector: selectAllProducts,
+              value: []
+            }
+          ]
+        })
+      ]
     })
-    .compileComponents();
+      .compileComponents();
 
+    store = TestBed.inject(MockStore);
     fixture = TestBed.createComponent(ProductsListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +48,38 @@ describe('ProductsListComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should not show any product on component initialization', () => {
+    testScheduler.run(({ cold, expectObservable }) => {
+      const expected = cold('a', { a: [] });
+      expectObservable(component.productList$).toEqual(expected);
+      const products = fixture.debugElement.queryAll(By.css('[data-cy]'));
+      expect(products).toHaveSize(0);
+    });
+  });
+
+  it('should show at least one product', () => {
+    testScheduler.run(({ cold, expectObservable }) => {
+      store.overrideSelector(selectAllProducts, [mockProduct]);
+      const expected = cold('a', { a: [mockProduct] });
+      expectObservable(component.productList$).toEqual(expected);
+      store.setState({ products: [mockProduct] });
+      fixture.detectChanges();
+      const products = fixture.debugElement.queryAll(By.css('[data-cy]'));
+      expect(products).toHaveSize(1);
+    });
+  });
+
+  it('should react to a product item click', () => {
+    store.overrideSelector(selectAllProducts, [mockProduct]);
+    store.setState({ products: [mockProduct] });
+    fixture.detectChanges();
+    const product = fixture.debugElement.query(By.directive(ProductShowcaseComponent));
+    expect(product).toBeTruthy();
+    spyOn(component, 'addToBasket');
+    (product.componentInstance as ProductShowcaseComponent).click.emit(mockProduct);
+    fixture.detectChanges();
+    expect(component.addToBasket).toHaveBeenCalledWith(mockProduct);
   });
 });
