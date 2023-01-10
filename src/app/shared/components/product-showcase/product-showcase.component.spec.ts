7042bb181967ec1b616eb413cf0194860c023d60
Christian Galvez <christian.galvez@unbounce.com>
2023-01-09 23:43:28 -0700

    Product showcase component unit testing

--- src/app/shared/components/product-showcase/product-showcase.component.spec.ts
+++ src/app/shared/components/product-showcase/product-showcase.component.spec.ts
@@ -1,14 +1,26 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProductShowcaseComponent } from './product-showcase.component';
+import { TestScheduler } from "rxjs/testing";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { Product } from "../../../core/models/product";
+import { By } from "@angular/platform-browser";
+import { ProductViews } from "../../../core/enums/product-views";
+
+const testScheduler = new TestScheduler((actual, expected) => {
+  expect(actual).toEqual(expected);
+});
 
 describe('ProductShowcaseComponent', () => {
   let component: ProductShowcaseComponent;
   let fixture: ComponentFixture<ProductShowcaseComponent>;
+  const mockedProduct: Product = { title: 'Product 1', brand: 'Brand 1', price: 59.99, sku: '73526212', stock: 1 };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ProductShowcaseComponent ]
+      declarations: [ ProductShowcaseComponent ],
+      imports: [MatButtonModule, MatIconModule]
     })
     .compileComponents();
 
@@ -19,5 +31,43 @@ describe('ProductShowcaseComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render the product name', () => {
+    component.product = mockedProduct;
+    fixture.detectChanges();
+    const productName = fixture.debugElement.query(By.css('.product__title'));
+    expect(productName).toBeTruthy();
+    expect(productName.nativeElement.innerText).toEqual(mockedProduct.title);
+  });
+
+  it('should render the product price', () => {
+    component.product = mockedProduct;
+    fixture.detectChanges();
+    const productPrice = fixture.debugElement.query(By.css('.product__price'));
+    expect(productPrice).toBeTruthy();
+    expect(productPrice.nativeElement.innerText).toEqual(`$${mockedProduct.price}`);
+  });
+
+  it('should render with the default view', () => {
+    expect(component.view).toEqual(ProductViews.default);
+  });
+
+  it('should hide the "Add to Basket" button when the view specified is Basket', () => {
+    component.view = ProductViews.basket;
+    fixture.detectChanges();
+    const addToBasketBtn = fixture.debugElement.query(By.css('[data-cy=addProductBasket]'));
+    const removeFromBasketBtn = fixture.debugElement.query(By.css('[data-cy=removeProductBasket]'));
+    expect(removeFromBasketBtn).toBeTruthy();
+    expect(addToBasketBtn).not.toBeTruthy();
+    expect(component.view).toEqual(ProductViews.basket);
+  });
+
+  it('should emit the product when clicking on it on default view', () => {
+    const addToBasketBtn = fixture.debugElement.query(By.css('[data-cy=addProductBasket]'));
+    spyOn(component, 'select');
+    spyOn(component.click, 'emit');
+    addToBasketBtn.triggerEventHandler('click');
+    expect(component.select).toHaveBeenCalled();
   });
 });
