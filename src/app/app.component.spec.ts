130bd8ba348b45d65e6cec3c41c1b373dbf3f56a
Christian Galvez <christian.galvez@unbounce.com>
2023-01-13 16:34:48 -0700

    Spinner component testing and remove unit tests from AppComponent

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -15,17 +15,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'marketplace2'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('marketplace2');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('marketplace2 app is running!');
-  });
 });
--- src/app/shared/components/spinner/spinner.component.spec.ts
+++ src/app/shared/components/spinner/spinner.component.spec.ts
@@ -1,17 +1,45 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SpinnerComponent } from './spinner.component';
+import { MockStore, provideMockStore } from "@ngrx/store/testing";
+import { Component } from "@angular/core";
+import { selectPendingState } from "../../../state/selectors/loader.selectors";
+import { TestScheduler } from "rxjs/testing";
+
+@Component({
+  selector: 'app-mock-container',
+  template: `
+    <app-spinner></app-spinner>
+  `
+})
+class MockContainerComponent {}
+
+const testScheduler = new TestScheduler((actual, expected) => {
+  expect(actual).toEqual(expected);
+});
 
 describe('SpinnerComponent', () => {
   let component: SpinnerComponent;
   let fixture: ComponentFixture<SpinnerComponent>;
+  let mockStore: MockStore;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SpinnerComponent ]
+      declarations: [ SpinnerComponent ],
+      providers: [
+        provideMockStore({
+          selectors: [
+            {
+              selector: selectPendingState,
+              value: true
+            }
+          ]
+        })
+      ]
     })
     .compileComponents();
 
+    mockStore = TestBed.inject(MockStore);
     fixture = TestBed.createComponent(SpinnerComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +47,20 @@ describe('SpinnerComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should show the spinner', () => {
+    testScheduler.run(({cold, expectObservable}) => {
+      const expected = cold('a', { a: true });
+      expectObservable(component.loading$).toEqual(expected);
+    });
+  });
+
+  it('should hide the spinner', () => {
+    testScheduler.run(({cold, expectObservable}) => {
+      mockStore.overrideSelector(selectPendingState, false);
+      const expected = cold('a', { a: false });
+      expectObservable(component.loading$).toEqual(expected);
+    });
   });
 });
