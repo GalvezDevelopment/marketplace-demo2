a7d891f99a0d082df772f9afd94311f0ac317bd6
Christian Galvez <christian.galvez@unbounce.com>
2023-01-13 17:33:12 -0700

    Flacky unit tests

--- src/app/features/checkout/checkout.component.spec.ts
+++ src/app/features/checkout/checkout.component.spec.ts
@@ -1,6 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CheckoutComponent } from './checkout.component';
+import { MockStore, provideMockStore } from "@ngrx/store/testing";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { MatButtonModule } from "@angular/material/button";
+import { RouterTestingModule } from "@angular/router/testing";
 
 describe('CheckoutComponent', () => {
   let component: CheckoutComponent;
@@ -8,7 +13,10 @@ describe('CheckoutComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CheckoutComponent ]
+      declarations: [ CheckoutComponent ],
+      imports: [
+        RouterTestingModule
+      ]
     })
     .compileComponents();
 
